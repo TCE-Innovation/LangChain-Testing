c2cd53b92173e0c20635561977dbe4b9d334baa2
RoryMO <120664694+RoryMO@users.noreply.github.com>
2023-09-13 10:37:05 -0400

    API Key Now Protected

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from langchain.prompts import ChatPromptTemplate
 from langchain.prompts.chat import SystemMessage, HumanMessagePromptTemplate
 from langchain.document_loaders import PyPDFLoader
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+openai_key = os.getenv("OPENAI_KEY")
+
 
 template = ChatPromptTemplate.from_messages(
     [
@@ -20,35 +26,6 @@ from langchain.chat_models import ChatOpenAI
 loader = PyPDFLoader("C32520_PRDC01 Scope of Work 2023.06.22.pdf")
 pages = loader.load_and_split()
 
-llm = ChatOpenAI(openai_api_key="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")
+llm = ChatOpenAI(openai_api_key=openai_key)
 print(llm(template.format_messages(text=pages[1].page_content)).content)
 print()
-
-# from langchain.prompts import ChatPromptTemplate
-# from langchain.prompts.chat import SystemMessage, HumanMessagePromptTemplate
-# from langchain.document_loaders import PyPDFLoader
-
-# template = ChatPromptTemplate.from_messages(
-#     [
-#         SystemMessage(
-#             content=(
-#                 "You are a helpful assistant that summarizes the user's input text."
-#             )
-#         ),
-#         HumanMessagePromptTemplate.from_template("{text}"),
-#     ]
-# )
-
-# from langchain.chat_models import ChatOpenAI
-
-# loader = PyPDFLoader("C32520_PRDC01 Scope of Work 2023.06.22.pdf")
-# pages = loader.load_and_split()
-
-# def get_user_input():
-#     """Get user input from the keyboard."""
-#     user_input = input("Enter the text you want to summarize: ")
-#     return user_input
-
-# llm = ChatOpenAI(openai_api_key="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")
-# print(llm(template.format_messages(text=get_user_input())).content)
-# print()
